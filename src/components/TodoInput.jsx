dcf20e6e291e3a4c25e274d5c32e8e4b666fdba3
Mahdi Falamarzi <mehdi.kindly@gmail.com>
2019-11-09 13:15:20 +0330

    add bootstrap and create components

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <h1>
+                Hello from Todo input
+
+            </h1>
+        )
+    }
+}
